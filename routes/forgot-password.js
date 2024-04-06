690e6dd71ef3503bbdb84d003216a5aac416e67b
abhi9572 <abhishekraj@57610@gmail.com>
2024-04-07 00:45:06 +0530

    some basic changes

--- routes/forgot-password.js
+++ routes/forgot-password.js
@@ -1,12 +1,10 @@
 const express = require("express");
-//const bcrypt = require('bcrypt');
 const router = express.Router();
 const userModel = require("../models/users");
 
 
 // Route to send JSON response for the forgot password page
 router.get("/forgot-password", (req, res) => {
-  // res.status(200).json({ success:'true', message: "Forgot password page accessed successfully" }); 
   res.render("forgot-password", { error: req.flash("error") });
 });
 
@@ -17,12 +15,10 @@ router.post("/forgot-password", async (req, res) => {
   try {
     // Check if newPassword and confirmPassword match
     if (!newPassword || !confirmNewPassword) {
-      // return res.status(400).json({ success: false, message: "Please fill all required fields" }); //in JSON Method Only FOR API
       req.flash("error", "Please fill in all required fields. ");
       return res.redirect("/forgot-password");
     }
     if (newPassword !== confirmNewPassword) {
-      // return res.status(400).json({ success: false, message: "Passwords do not match" }); //in JSON Method Only FOR API
       req.flash("error", "Passwords do not match.");
       return res.redirect("/forgot-password");
     }
@@ -31,7 +27,6 @@ router.post("/forgot-password", async (req, res) => {
     const user = await userModel.findOne({ email });
 
     if (!user) {
-      // return res.status(404).json({ success: false, message: "User not found" }); //in JSON Method Only FOR API
       req.flash("error", "User not found.");
       return res.redirect("/forgot-password");
     }
@@ -44,7 +39,6 @@ router.post("/forgot-password", async (req, res) => {
     });
   } catch (error) {
     console.error(error);
-    // return res.status(500).json({ success: false, message: "Internal server error" });  //in JSON Method Only FOR API
     req.flash("error", "Internal server error");
     return res.redirect("/forgot-password");
   }
